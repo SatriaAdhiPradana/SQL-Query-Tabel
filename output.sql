ac1f3022bcdf93e7d4d9d3201a0132fae9bc23b7
Satria Adhi Pradana <92844616+SatriaAdhiPradana@users.noreply.github.com>
2023-02-17 14:14:22 +0700

    Create output.sql

---
+++ output.sql
@@ -0,0 +1,6 @@
+name          |  product      |  quantity
+--------------|---------------|----------
+John Smith    |  T-Shirt      |  2
+Jane Doe      |  Jeans        |  1
+John Smith    |  Hoodie       |  1
+Mark Johnson  |  Jacket       |  3
