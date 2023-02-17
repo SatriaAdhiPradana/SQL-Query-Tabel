d3aec81b67d54e8aa2b4b9bfe5d59dcf29af91dd
Satria Adhi Pradana <92844616+SatriaAdhiPradana@users.noreply.github.com>
2023-02-17 14:10:29 +0700

    Create Tabel-customer.sql

---
+++ Tabel-customer.sql
@@ -0,0 +1,5 @@
+id  |  name          |  email
+----|----------------|-------------------------
+1   |  John Smith    |  johnsmith@gmail.com
+2   |  Jane Doe      |  janedoe@yahoo.com
+3   |  Mark Johnson  |  markjohnson@hotmail.com
