17fc665815ad0e9c7e51a94d41eba722daf64e04
Satria Adhi Pradana <92844616+SatriaAdhiPradana@users.noreply.github.com>
2023-02-17 14:11:50 +0700

    Create Tabel-order.sql

---
+++ Tabel-order.sql
@@ -0,0 +1,6 @@
+id  |  customer_id  |  product      |  quantity
+----|--------------|---------------|----------
+1   |  1           |  T-Shirt      |  2
+2   |  2           |  Jeans        |  1
+3   |  1           |  Hoodie       |  1
+4   |  3           |  Jacket       |  3
